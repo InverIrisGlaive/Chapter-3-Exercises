e57ccce2a50a909937b2550723c301a620ea65e0
InverIrisGlaive <38737968+InverIrisGlaive@users.noreply.github.com>
2021-09-27 13:06:22 -0300

    added a comment about output.

--- src/CalculateFinalGrade.java
+++ src/CalculateFinalGrade.java
@@ -31,12 +31,12 @@ public class CalculateFinalGrade {
     public static void main(String[] args) { //You can also just type "throws" here and pass the exception up, and not type a try...catch statement
         //but this means this method won't deal with the exception thrown in the CS2910 method. if you want to solve the exception thrown in the
         //cs2910 method, you don't type "throws", and you type a try catch statement here
-        int[] grades = {67, 98, 78, 65, 45, 49, 56, 72, -1}; //{23, 34, 78, 35, 68, 59};
+        int[] grades = {67, 98, 78, 65, 45, 49, 56, 72, -1};
         try {// this try...catch statement will solve the exception raised in the CS1920 class (e.g. neg grade, or less than 9 grades)
             System.out.println("The final grade is " + CS2910.CalculateFinalGrade(grades));
         } catch (Exception e) {
             System.out.println(e); // you can call a method in the same package if the method is a static method without actually having to instantiate
-            //an object here. So just call it. Here the variable e represents the types of exception object thrown in the method CS2910.
-        }
+A        }// it's supposed to throw an exception error b/c one of the grade is a negative number
+        // result: "java.lang.Exception: Grades must be between 0 and 100"
     }
 }
